91fe3e9e522fda04b00db9cf3495f46de09fbef4
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2026-03-26 20:58:39 -0400

    serve the right page

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(`Received request for ${req.url}`);
-    res.end('check terminal');
+   if (req.url === '/') {
+        res.end('Welcome to the Home Page');
+   } else if (req.url === '/about') {
+        res.end('Welcome to the About Page');
+   } else if (req.url === '/contact-me') {
+        res.end('Welcome to the Contact Page');
+   } else {
+         
+        res.end('404 - Page Not Found');
+   }
 });
 
-server.listen(8080, () => {
-    console.log("Server is running on port 8080");
-});
+server.listen(8080);
