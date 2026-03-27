2549b3aae5e1d3d898b0e7585e66d0c8805cc3be
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2026-03-26 21:03:12 -0400

    add headers

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
    if (req.url === '/') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(fs.readFileSync('index.html'));     
    } else if (req.url === '/about') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(fs.readFileSync('about.html'));     
    } else if (req.url === '/contact-me') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(fs.readFileSync('contact-me.html'));     
    } else {
+        res.writeHead(404, {'Content-Type': 'text/html'});
         res.end(fs.readFileSync('404.html'));
    }
 });
